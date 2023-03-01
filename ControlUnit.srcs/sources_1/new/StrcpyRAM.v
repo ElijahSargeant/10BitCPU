6fae0836e7a6f774aa57ad8d6bb778920e147677
Vito <torinavito5@gmail.com>
2023-03-01 12:05:28 -0500

    Sup, I made then mult..... now test it

--- ControlUnit.srcs/sources_1/new/StrcpyRAM.v
+++ ControlUnit.srcs/sources_1/new/StrcpyRAM.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 01:15:53 PM
-// Design Name: 
-// Module Name: StrcpyRAM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module StrcpyRAM(output reg [9:0] readData,
         input [9:0] writeData,
@@ -27,6 +7,11 @@ module StrcpyRAM(output reg [9:0] readData,
         input clk);
         
     reg [9:0] memory[511:0]; //512 positions of 10 bit memory
+
+    initial begin
+        memory[0] <= 'b0000000011; // 3
+        memory[1] <= 'b0000001010; // 10
+     end
      
     always@(posedge clk) begin
         if(writeEnable == 1) begin
--- ControlUnit.srcs/sources_1/new/StrcpyROM.v
+++ ControlUnit.srcs/sources_1/new/StrcpyROM.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 01:04:08 PM
-// Design Name: 
-// Module Name: StrcpyROM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module StrcpyROM(output reg [9:0] readData,
@@ -27,21 +8,23 @@ module StrcpyROM(output reg [9:0] readData,
     reg [9:0] memory[511:0]; //512 positions of 10 bit memory
     
     initial begin
-        memory[0] <= 'b1001111000;
-        memory[1] <= 'b1001110000;
-        memory[2] <= 'b0000010011;
-        memory[3] <= 'b0001111001;
-        memory[4] <= 'b0100100011;
-        memory[5] <= 'b1011000111;
-        memory[6] <= 'b1110000010;
-        memory[7] <= 'b0001010100;
-        memory[8] <= 'b1111111111;
-        memory[9] <= 'b1111111111;
-        memory[10] <= 'b1111111111;
-        memory[11] <= 'b1111111111;
-        memory[12] <= 'b1111111111;
-        memory[13] <= 'b1111111111;
-        memory[14] <= 'b1111111111;
+        memory[0] <= 'b1101000000; // load x
+        memory[1] <= 'b1101100001; // load y 
+        memory[2] <= 'b0101011000; // load x into i0 
+        memory[3] <= 'b0101100001; // load y into i1
+        memory[4] <= 'b1001111000; // load immediate g5 with 0
+        memory[5] <= 'b1001110000; // load immediate g4 with 0 
+        memory[6] <= 'b1001010000; // load immediate g0 with 0 
+        memory[7] <= 'b0000010011; // add g1 to g0
+        memory[8] <= 'b0001110111; // add -1 to g4
+        memory[9] <= 'b0001111001; // add 1 to g5
+        memory[10] <= 'b0100100111; // cmp g2 to g5
+        memory[11] <= 'b1011001111; // beq exit
+        memory[12] <= 'b0100100110; // cmp g2 to g4
+        memory[13] <= 'b1011001111; // beq exit 
+        memory[14] <= 'b1110000111; // j mult
+        memory[15] <= 'b0001010100; // sub 4 
+        memory[16] <= 'b1111111111; // halt! 
     end 
     
     always@(*) begin
