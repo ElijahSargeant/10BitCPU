2a4fb9228c4e5f6e18d043f1c875d90820ba9b30
Dylan Garza <dylangarza1909@gmail.com>
2023-03-01 13:07:55 -0500

    string copied into RAM

--- ControlUnit.srcs/sources_1/new/StrcpyRAM.v
+++ ControlUnit.srcs/sources_1/new/StrcpyRAM.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 01:15:53 PM
-// Design Name: 
-// Module Name: StrcpyRAM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module StrcpyRAM(output reg [9:0] readData,
         input [9:0] writeData,
@@ -27,6 +7,20 @@ module StrcpyRAM(output reg [9:0] readData,
         input clk);
         
     reg [9:0] memory[511:0]; //512 positions of 10 bit memory
+
+    initial begin
+
+        memory[0] <= 'b0001100010; // b
+        memory[1] <= 'b0001100001; // a
+        memory[2] <= 'b0000100000; // 
+        memory[3] <= 'b0001101000; // h
+        memory[4] <= 'b0001110101; // u
+        memory[5] <= 'b0001101101; // m
+        memory[6] <= 'b0001100010; // b
+        memory[7] <= 'b0001110101; // u
+        memory[8] <= 'b0001100111; // g
+        memory[9] <= 'b0000000000;
+    end
      
     always@(posedge clk) begin
         if(writeEnable == 1) begin
