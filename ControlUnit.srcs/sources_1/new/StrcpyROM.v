9f2d4274c01facf823b11ec4fc23b7321fb60c25
Dylan Garza <dylangarza1909@gmail.com>
2023-03-01 12:55:18 -0500

    strcopy instructions written into rom

--- ControlUnit.srcs/sources_1/new/StrcpyROM.v
+++ ControlUnit.srcs/sources_1/new/StrcpyROM.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/23/2023 01:04:08 PM
-// Design Name: 
-// Module Name: StrcpyROM
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module StrcpyROM(output reg [9:0] readData,
@@ -27,21 +8,23 @@ module StrcpyROM(output reg [9:0] readData,
     reg [9:0] memory[511:0]; //512 positions of 10 bit memory
     
     initial begin
-        memory[0] <= 'b1001111000;
-        memory[1] <= 'b1001110000;
-        memory[2] <= 'b0000010011;
-        memory[3] <= 'b0001111001;
-        memory[4] <= 'b0100100011;
-        memory[5] <= 'b1011000111;
-        memory[6] <= 'b1110000010;
-        memory[7] <= 'b0001010100;
-        memory[8] <= 'b1111111111;
-        memory[9] <= 'b1111111111;
-        memory[10] <= 'b1111111111;
-        memory[11] <= 'b1111111111;
-        memory[12] <= 'b1111111111;
-        memory[13] <= 'b1111111111;
-        memory[14] <= 'b1111111111;
+        memory[0] <= 'b1001111000; // load 0 into g5
+        memory[1] <= 'b1001110000; // load 0 into g4
+        memory[2] <= 'b1101100000; // lmao i1 <= M{00000}
+        memory[3] <= 'b1101010000; // lmao i0 <= M{10000}
+        memory[4] <= 'b0101010001; // lw M[i1] to g1
+        memory[5] <= 'b0110010000; // smh i0 <= g0
+        memory[6] <= 'b0001110001; // addi g4 += 1
+        memory[7] <= 'b0001000001; // addi i0 += 1
+        memory[8] <= 'b0001001001; // addi i1 += 1
+        memory[9] <= 'b1000001111; // cmp i1 to g5
+        memory[10] <= 'b1011001100; // beq 001100 done
+        memory[11] <= 'b1110000100; // jmp loop
+        memory[12] <= 'b1101111000; // lmao i1 11000
+        memory[13] <= 'b0101101001; // lw M[i1] to g3
+
+        memory[14] <= 'b1111111111; // halt
+
     end 
     
     always@(*) begin
